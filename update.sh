b669801f5f850ba61da1c6b3a6288a8c1ba62a02
Anton Kolechkin <anton.kolechkin@gmail.com>
2014-07-16 01:20:37 +0700

    cv

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ls -1 . | grep -v -E "content|other|update.sh" | xargs rm -rf && cyrax content && mv content/_build/* .
+ls -1 . | grep -v -E "content|other|update.sh|CNAME" | xargs rm -rf && cyrax content && mv content/_build/* .
 
